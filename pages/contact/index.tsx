467d78f5ec467f7eb765d857de482049559fc017
suman <suman@biswas.com>
2022-02-26 15:04:34 +0530

    1.0.0dev

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -4,10 +4,104 @@ import { FiFacebook } from "react-icons/fi";
 import { AiOutlineTwitter } from "react-icons/ai";
 import NextLink from "next/link";
 import { NavButton } from "../../components/NavButton";
+import { useRef, useState } from "react";
+import { getDatabase, ref, push } from "firebase/database";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../../firebase/app";
+
+initializeApp(firebaseConfig);
+
 export default function Contact() {
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const messageRef = useRef<HTMLTextAreaElement | null>(null);
+  function handleSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    let name = nameRef.current?.value;
+    let email = emailRef.current?.value;
+    let message = messageRef.current?.value;
+    if (!name || !email || !message) {
+      toast.info("Please fill the form correctly!", {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      console.log("Please fill the form correctly");
+    } else {
+      var validRegex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+      if (email.match(validRegex)) {
+        const db = getDatabase();
+        push(ref(db, "messages/"), {
+          name,
+          email,
+          message,
+        })
+          .then(() => {
+            toast.success("Message sent successfully", {
+              position: "top-center",
+              autoClose: 1000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            });
+            console.log("Message sent successfully");
+            emailRef.current!.style.outlineColor = "#000";
+            nameRef.current!.value = "";
+            emailRef.current!.value = "";
+            messageRef.current!.value = "";
+          })
+          .catch((e) => {
+            toast.error("Message not sent", {
+              position: "top-center",
+              autoClose: 1000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            });
+            console.log(e);
+          });
+      } else {
+        emailRef.current?.focus();
+        emailRef.current!.style.outlineColor = "#cf3a3a";
+        toast.warn("Please provide a valid email", {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        console.log("please use a valid email");
+      }
+    }
+  }
+
   return (
     <div className={styles.main_container}>
       <NavButton />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div className={styles.pagename}>
         <p className={styles.pagename_name}>contact</p>
         <div className={styles.pagename_border} />
@@ -15,21 +109,26 @@ export default function Contact() {
       <form className={styles.form}>
         <h3 className={styles.form_heading}>Contact Me</h3>
         <input
+          ref={nameRef}
           type={"text"}
           placeholder="your name"
           className={styles.form_input}
         />
         <input
+          ref={emailRef}
           type={"email"}
           placeholder="your email"
           className={styles.form_input}
         />
         <textarea
+          ref={messageRef}
           placeholder="Message"
           className={`${styles.form_input} ${styles.form_message}`}
         />
         <div className={styles.form_flex}>
-          <button className={styles.form_submit_btn}>Send</button>
+          <button onClick={handleSubmit} className={styles.form_submit_btn}>
+            Send
+          </button>
           <div className={styles.form_flex_icons}>
             <NextLink href={"/"}>
               <div className={styles.form_icons_box}>
