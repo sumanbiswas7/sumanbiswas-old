4fb26ab786a22c0a550d515e496cfe77a17a4345
suman <ronaldosuman97@gmail.com>
2022-06-19 13:23:34 +0530

    cv url fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,7 +69,7 @@ const Home: NextPage = () => {
               <Button text="hire me" href="/contact" style="dark" />
               <Button
                 text="resume"
-                href="https://drive.google.com/file/d/1gbo8yzCoMlM6foz54ZSEXhB4ApDX8YAY/view?usp=sharing"
+                href="https://drive.google.com/file/d/1Y5zHzuHRS4Y7G3njoa2sUIjUoX0PyDbQ/view?usp=sharing"
                 style="white"
               />
             </div>
